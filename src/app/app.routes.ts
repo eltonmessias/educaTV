e1c14b16ee252d11488ba30d2cdbaa57f024b05a
elton <eltonguambe1006@gmail.com>
2025-04-11 00:16:08 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component:HomeComponent},
+    {path: 'header', component:HeaderComponent}
+];
