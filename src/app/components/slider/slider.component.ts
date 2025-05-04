9a061309c8f96bca09304ae505bfb9f05687d594
elton <eltonguambe1006@gmail.com>
2025-05-04 15:21:25 +0200

    feat:versao final 1.0

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -16,16 +16,55 @@ export class SliderComponent implements OnInit, OnDestroy{
   currentSlide: number = 0;
   private slideInterval$: Subscription = new Subscription;
 
+  // ngOnInit(): void {
+  //   // A cada 2 segundos, atualiza o índice do slide
+  //   this.slideInterval$ = interval(3000).subscribe(() => {
+  //     this.currentSlide = (this.currentSlide + 1) % this.images.length;
+  //   });
+  // }
+
+  // ngOnDestroy(): void {
+  //   if (this.slideInterval$) {
+  //     this.slideInterval$.unsubscribe();
+  //   }
+  // }
+
+  currentIndex = 1;
+  intervalId: any;
+  transitioning = true;
+
   ngOnInit(): void {
-    // A cada 2 segundos, atualiza o índice do slide
-    this.slideInterval$ = interval(3000).subscribe(() => {
-      this.currentSlide = (this.currentSlide + 1) % this.images.length;
-    });
+    this.startAutoSlide();
   }
 
   ngOnDestroy(): void {
-    if (this.slideInterval$) {
-      this.slideInterval$.unsubscribe();
-    }
+    clearInterval(this.intervalId);
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }
+
+  nextSlide() {
+    this.currentIndex++;
+    this.transitioning = true;
+
+    // Reset para o início após última imagem clone
+    setTimeout(() => {
+      if (this.currentIndex === this.images.length + 1) {
+        this.transitioning = false;
+        this.currentIndex = 1;
+      }
+    }, 500); // deve coincidir com o tempo da transição
+  }
+
+  getTransform(): string {
+    return `translateX(-${this.currentIndex * 100}%)`;
+  }
+
+  getTransition(): string {
+    return this.transitioning ? 'transform 2s ease-in-out' : 'none';
   }
 }
